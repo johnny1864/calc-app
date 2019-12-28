f33034321e6b65d6167a89e169ca501ecc236427
powerdigital-dev <johnny1864@yahoo.com>
2019-12-27 18:30:50 -0800

    added property

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./form.component.scss']
 })
 export class FormComponent implements OnInit {
-
+  other: false;
   constructor() { }
 
   ngOnInit() {
